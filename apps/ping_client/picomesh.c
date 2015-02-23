fd965944014ea6c3e34da92d5d5af3708279da2b
Daniele Lacamera <daniele.lacamera@tass.be>
2015-02-23 11:07:49 +0100

    Added support for picoTCP's AODV test

--- apps/ping_client/picomesh.c
+++ apps/ping_client/picomesh.c
@@ -4,8 +4,10 @@
 #include "pico_olsr.h"
 #include "pico_ipv4.h"
 #include "pico_icmp4.h"
+#include "pico_socket.h"
 
 #define NUM_PING 1
+static int first_node = 0;
 
 
 void cb_ping(struct pico_icmp4_stats *s)
@@ -20,6 +22,31 @@ void cb_ping(struct pico_icmp4_stats *s)
     }
 }
 
+void cb(uint16_t ev, struct pico_socket *s)
+{
+    (void) ev;
+    (void) s;
+}
+
+void aodv_explore(pico_time now, void *arg)
+{
+    union pico_address dest;
+    struct pico_socket *s;
+    char hello[6] = "hello";
+    int ret;
+    pico_string_to_ipv4("10.42.0.10", &dest.ip4.addr);
+    (void)arg;
+
+
+    if (first_node) {
+        s = pico_socket_open(PICO_PROTO_IPV4, PICO_PROTO_UDP, cb);
+        ret = pico_socket_sendto(s, hello, 5, &dest, short_be(5555));
+        printf("Sending explore hello packet via udp: %d (%s)\n", ret, strerror(pico_err));
+        pico_socket_close(s);
+        pico_timer_add(10000, aodv_explore, NULL);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     struct pico_device *dev;
@@ -29,11 +56,11 @@ int main(int argc, char *argv[])
     struct pico_ip4 address, netmask;
     struct pico_tree_node *index;
     struct pico_ipv4_route * entry;
-    char addr[16];
+    char addr[16], gw[16], nm[16];
     uint16_t interval = 1000;
     uint16_t timeout  = 9999;
     uint8_t size  = 28;
-    uint32_t metric  = 0;
+    uint32_t metric;
     uint8_t numping  = 1;
 
 
@@ -51,13 +78,24 @@ int main(int argc, char *argv[])
         metric = 0;
 
 
+
+    if (id == 1)
+        first_node = 1;
+
     dev = pico_geomess_create(id, x, y, range_max, range_good);
-    pico_string_to_ipv4("255.255.0.0", &netmask.addr);
+    pico_string_to_ipv4("255.255.255.0", &netmask.addr);
     pico_string_to_ipv4("10.42.0.0", &address.addr);
     address.addr += ntohl((uint32_t)(id & 0xFF));
     address.addr += ntohl((uint32_t)(id & 0xFF00));
+    //address.addr += ntohl((uint32_t)(id & 0xFF)<<16);
     pico_ipv4_link_add(dev, address, netmask);
+
+#ifdef OLSR
     pico_olsr_add(dev);
+#else
+    pico_aodv_add(dev);
+    pico_timer_add(1000, aodv_explore, NULL);
+#endif
 
     for(;;) {
         pico_stack_tick();
@@ -70,12 +108,15 @@ int main(int argc, char *argv[])
             if (metric!=0) {
                 dbg("timer passed\n");
                 pico_tree_foreach(index, &Routes) {
-                    dbg("Entry\n");
                     entry = index->keyValue;
-                    if (entry->metric == metric) {
-                        pico_ipv4_to_string(addr, entry->dest.addr);
-                        dbg("Ping to %s\n",addr);
+                    pico_ipv4_to_string(addr, entry->dest.addr);
+                    pico_ipv4_to_string(nm, entry->netmask.addr);
+                    pico_ipv4_to_string(gw, entry->gateway.addr);
+                    dbg("Entry %s/%s via %s metric %hu\n", addr, nm, gw, entry->metric);
+                    if (entry->netmask.addr == 0xFFFFFFFF && (entry->metric == metric)) { 
+                        dbg("Ping to %s via %s\n",addr, gw);
                         pico_icmp4_ping(addr, NUM_PING, interval, timeout, size, cb_ping);
+                        break;
                     }
                 }
             }
